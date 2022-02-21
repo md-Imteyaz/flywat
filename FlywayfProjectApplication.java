36d235a8b2e09ad181d673d818affcd3b814d6da
md-Imteyaz <98022042+md-Imteyaz@users.noreply.github.com>
2022-02-21 10:24:28 +0530

    Add files via upload

---
+++ FlywayfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.flywaypack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayfProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayfProjectApplication.class, args);
+	}
+
+}
